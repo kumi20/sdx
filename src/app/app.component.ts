974c9f0839652406ddb92622d0ebf33b25d82e45
Jakub Kumięga <jkumiega@csgroup.pl>
2018-03-23 12:10:13 +0100

    projekt startowy do tworzenia stron internetowych

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EventService } from './event.service'
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+    
+    constructor(private toastrService: EventService) {}
+    
+    test(){
+        this.toastrService.wyswietlInfo('success', 'test error');
+    }
+    
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA  } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { MDBBootstrapModulePro } from './typescripts/pro';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+import { ApiService } from './api.service';
+import { EventService } from './event.service';
+import { routerModule} from './app.routing';
+
+import { DashboardComponent} from './dashboard/dashboard.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routerModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    MDBBootstrapModulePro.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ApiService, EventService],
+  bootstrap: [AppComponent],
+  schemas: [ NO_ERRORS_SCHEMA ]
 })
 export class AppModule { }
