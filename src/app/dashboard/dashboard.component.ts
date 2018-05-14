67053f49dc67d072de2b41314827032d8b7fd7ad
Jakub Kumięga <jkumiega@csgroup.pl>
2018-05-14 14:46:50 +0200

    poprawa routingu

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,21 +1,40 @@
-import { Component, OnInit, ElementRef, Input, ViewChild, Output, EventEmitter } from '@angular/core';
-import { EventService } from '../event.service'
+import { Component, OnInit, ElementRef, Input, ViewChild, Output, EventEmitter, ViewChildren, AfterViewInit, OnDestroy} from '@angular/core';
+import { EventService } from '../event.service';
+import { ApiService } from '../api.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit, OnDestroy{
 
-  constructor(private toastrService: EventService) { }
-
-
+  @ViewChildren('dynamiCom') dynamiCom;  
+  id: number = 1;
+  private sub: any;
+    
+    
+  constructor(private CmsService: ApiService, private event: EventService, private route: ActivatedRoute, private _route: Router) { }
+    
   ngOnInit() {
-     
+      this.sub = this.route.params.subscribe(params => {
+        this.id = +params['id'];
+            if (isNaN(this.id)) this.id = 1;
+            if(this.dynamiCom != null){
+                this.ngAfterViewInit();
+            }
+      });
   }
-
-    test(){
-        this.toastrService.wyswietlInfo('success', 'test error');
-    }
+    
+  ngAfterViewInit(){
+      this.dynamiCom.forEach(el=>{
+          el.pobierzKontrolki(this.id);
+      })
+  }
+    
+ ngOnDestroy(){
+     this.sub.unsubscribe();
+ }    
+    
 }
